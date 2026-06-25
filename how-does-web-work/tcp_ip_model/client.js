b5d21bef169772baeb78d787a91a6c2be76ac157
Brian Dionisio <bdboyxg.555@gmail.com>
2026-06-25 23:03:11 +0800

    docs + feat: breakdown TCP handshake problem & import net module to build TCP server

---
+++ how-does-web-work/tcp_ip_model/client.js
@@ -0,0 +1,4 @@
+// initiator
+
+// here is the toolkit to allow for asynchronous networking API
+const net = require('net'); 
---
+++ how-does-web-work/tcp_ip_model/server.js
@@ -0,0 +1,6 @@
+// listener
+
+// here is the toolkit to allow for asynchronous networking API
+const net = require('node: net'); 
+
+
