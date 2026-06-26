223e69ede656d2f56ffd38f6f4c732213057c7bf
Brian Dionisio <bdboyxg.555@gmail.com>
2026-06-26 23:31:43 +0800

    feat: establish connection

--- how-does-web-work/tcp_ip_model/client.js
+++ how-does-web-work/tcp_ip_model/client.js
@@ -1,4 +1,12 @@
 // initiator
 
 // here is the toolkit to allow for asynchronous networking API
-const net = require('net'); 
+const net = require('node: net'); 
+
+// make client initiate connection
+const port = 3000;
+const host = '127.0.0.1';
+const client = net.createConnection(port, host, ()=> {
+    console.log('Client connected');
+
+});
--- how-does-web-work/tcp_ip_model/server.js
+++ how-does-web-work/tcp_ip_model/server.js
@@ -4,3 +4,15 @@
 const net = require('node: net'); 
 
 
+// build the TCP server, establish connection => fire connection event = socket object created 
+const server = net.createServer((socket) => {
+    console.log('Client connected');
+    
+
+});
+
+// bind a server to an entrypoint 
+const port = 3000;
+const host = '127.0.0.1';
+server.listen(port, host);
+
