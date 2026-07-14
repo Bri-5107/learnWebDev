fbbdbdd751f81d02ba3dd8738b2e0d2d33a05f73
Brian Dionisio <bdboyxg.555@gmail.com>
2026-07-14 18:20:44 +0800

    feat: create backedn to listen and reply for request

---
+++ how-does-web-work/http_request/js/corsBack.js
@@ -0,0 +1,47 @@
+import http, { get } from 'node:http';
+
+
+// 1. Setup Server 
+const hostname = '127.0.0.1';
+const port = '8080';
+
+
+const server = http.createServer((request, response) => {
+    //Check URL
+    if (request.method === 'GET' && request.url === '/simple'){
+        //Apply CORS Header
+        response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+        response.statusCode = 200;
+
+        //Reply
+        response.write('Hello, Frontend!')
+    } else {
+        response.statusCode = 404; 
+    }
+
+    // End connection
+    response.end();
+}); 
+
+// Listen for incoming requests
+server.listen(8080);
+
+
+
+
+
+
+/**
+ * PLAN:
+ * 1. Setup the server/Establish Connection 
+ * 2. Setup callback
+ *    - Check URL 
+ *    - Apply CORS header 
+ *    - Reply 
+ * 4. Establish Connection/Listen 
+ * 
+ * 
+ * ACTIVE RECALL QUESTIONS:
+ * - What are the different modules you can use in order for an HTTP backend server to receive a HTTP frontend request
+ * 
+ */
