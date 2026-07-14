fb79891965f517591eefa81674ec469106c3aa85
Brian Dionisio <bdboyxg.555@gmail.com>
2026-07-14 19:44:17 +0800

    feat: create front end HTTP request

--- how-does-web-work/http_request/js/corsFront.js
+++ how-does-web-work/http_request/js/corsFront.js
@@ -1,14 +1,21 @@
+// get the button from HTML using document object 
 const simpleButton = document.getElementById("simpleButton");
 
-function sendSimpleRequest(event){
+// function parameter for the addEventListener function(event, function)
+async function sendSimpleRequest(event){
     try {
-        fetch();
+       const fetchRequest = await fetch('http://127.0.0.1:8080/simple');
+       const message = await fetchRequest.text();
+       console.log(message);
+
+        
     } catch (error) {
+        console.error('Fetch failed:', error);
 
     }
     
 };
-
+// attach Event Listener to the button
 simpleButton.addEventListener("click", sendSimpleRequest);
 
 
@@ -20,6 +27,12 @@ simpleButton.addEventListener("click", sendSimpleRequest);
 
 
 /**
+ * PLAN
+ * 1. get button
+ * 2. attach event listener
+ * 3. create a function that will send an HTTP simple request to a backend server (node.js)
+ * 
+ * 
  * ACTIVE RECALL QUESTIONS:
  * - What is the DOM 
  * - What is the difference of a function with and without parenthesis? 
