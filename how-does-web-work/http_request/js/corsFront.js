f4813484feb3e635efba91e90a35c699c753f343
Brian Dionisio <bdboyxg.555@gmail.com>
2026-07-14 14:18:36 +0800

    feat: setup simple request feature

---
+++ how-does-web-work/http_request/js/corsFront.js
@@ -0,0 +1,30 @@
+const simpleButton = document.getElementById("simpleButton");
+
+function sendSimpleRequest(event){
+    try {
+        fetch();
+    } catch (error) {
+
+    }
+    
+};
+
+simpleButton.addEventListener("click", sendSimpleRequest);
+
+
+
+
+
+
+
+
+
+/**
+ * ACTIVE RECALL QUESTIONS:
+ * - What is the DOM 
+ * - What is the difference of a function with and without parenthesis? 
+ * - How to build a fetch request 
+        - What are the parameters, return object? 
+        - How to build try and catch-response 
+ *      - 
+ */
