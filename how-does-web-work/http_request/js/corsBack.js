4f402a9ac3a17638d064b858e225456e5316c22f
Brian Dionisio <bdboyxg.555@gmail.com>
2026-07-14 20:23:29 +0800

    fix: change port number

--- how-does-web-work/http_request/js/corsBack.js
+++ how-does-web-work/http_request/js/corsBack.js
@@ -3,7 +3,7 @@ import http, { get } from 'node:http';
 
 // 1. Setup Server 
 const hostname = '127.0.0.1';
-const port = '8080';
+const port = '3000';
 
 
 const server = http.createServer((request, response) => {
@@ -24,7 +24,7 @@ const server = http.createServer((request, response) => {
 }); 
 
 // Listen for incoming requests
-server.listen(8080);
+server.listen(3000);
 
 
 
