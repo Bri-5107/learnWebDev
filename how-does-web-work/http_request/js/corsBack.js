2475703bc17e74611687e32a1753807d2f1bacee
Brian Dionisio <bdboyxg.555@gmail.com>
2026-07-15 16:03:26 +0800

    feat: add preflight request receiver

--- how-does-web-work/http_request/js/corsBack.js
+++ how-does-web-work/http_request/js/corsBack.js
@@ -14,7 +14,19 @@ const server = http.createServer((request, response) => {
         response.statusCode = 200;
 
         //Reply
-        response.write('Hello, Frontend!')
+        response.write('Hello, Frontend!');
+
+    } else if (request.method === 'OPTIONS' && request.url === '/flight'){
+        
+        response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+        response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        response.statusCode = 200;
+    
+    } else if (request.method === 'GET' && request.url === '/flight') {
+        response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+        response.statusCode = 200;
+        response.write('Hello again, Frontend! The preflight was successful!');
     } else {
         response.statusCode = 404; 
     }
@@ -47,6 +59,8 @@ server.listen(3000);
  * - What is the key response to a CORS client
  * - How to properly choose port
  * - Explain the role of FoxProxy and Caido 
+ *     - what is nip.io for 
+ *
  * 
  * 
  */
