899ae9f42f0c7852eedb403a8b475b9e219d2e5a
Brian Dionisio <bdboyxg.555@gmail.com>
2026-08-08 10:56:23 +0800

    wip: get DOM element selections for routing

---
+++ how-does-web-work/routing/routingClient.js
@@ -0,0 +1,23 @@
+const staticButton = document.getElementById("staticButton");
+const parameterButton = document.getElementById("parameterButton");
+const queryField = document.getElementById("queryField");
+const queryButton = document.getElementById("queryButton");
+const v1Button = document.getElementById("btn-v1");
+const v2Button = document.getElementById("btn-v2");
+const nestedButton = document.getElementById("nestedButton");
+const catchAllButton = document.getElementById("catchAllButton");
+
+
+//get response section
+const routeResponse = document.getElementById("routeResponse");
+/**
+ * Active Recall Questions: 
+ * 
+ * 
+ * 
+ * 
+ * 1. Add event listener 
+ * 2. On its callback function, How do I shape the request line route based on the type of routing? 
+ *
+ * 
+ */
